be0a9a03e1b8fa3f134056271b2a25ec0ed8c4df
rvansant2 <rvansant2@gmail.com>
2020-11-04 10:09:15 -0500

    feat(mvp): initial commit, start

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 
-function App() {
+import Layout from './pages/Layout';
+import Main from './components/Main';
+import NavBar from './components/NavBar';
+import Routes from './Routes';
+import defaultTheme from './themes/default';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React!!
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={defaultTheme}>
+      <Layout>
+        <Main maxWidth={false}>
+          <NavBar />
+          <Routes />
+        </Main>
+      </Layout>
+    </MuiThemeProvider>
   );
-}
+};
 
 export default App;
